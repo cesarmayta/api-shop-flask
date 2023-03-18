8833f2b718f68d74ccdaa12f9ae5e444a0878c38
cesar mayta <cesarmayta@gmail.com>
2023-03-18 11:52:24 -0500

    proyecto base

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from flask import Flask
 
 from .blueprints.shop import shop #Aquí importamos el blueprint 
 
-
+from .config import Config
 
 def create_app():
 
     app = Flask(__name__)
+
+    app.config.from_object(Config)
 
     app.register_blueprint(shop)
 
---
+++ app/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/db_shop'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
