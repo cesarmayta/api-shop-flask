e454f0ce1b7250b2c1979e1d2e1b66ccb12d81d7
César Mayta <cesarmayta@gmail.com>
2023-03-21 19:26:57 -0500

    variables de entorno

--- app/config.py
+++ app/config.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/db_shop'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
