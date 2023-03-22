909fd402233f88eb4890a9e1c6b22d2857dcc777
César Mayta <cesarmayta@gmail.com>
2023-03-21 20:30:07 -0500

    login

--- app/blueprints/auth/resources/usuario.py
+++ app/blueprints/auth/resources/usuario.py
@@ -13,7 +13,7 @@ api = Api(auth)
 
 class UsuarioResource(Resource):
     
-    @jwt_required
+    @jwt_required()
     def get(self):
         
         data = Usuario.get_all()
@@ -45,6 +45,31 @@ class UsuarioResource(Resource):
         }
         
         return context
+    
+class LoginResource(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        
+        objUsuario = Usuario.query.filter_by(username=username)
+        
+        if check_password_hash(objUsuario[0].password,password):
+            payload = {
+                'id': objUsuario[0].id,
+                'username':objUsuario[0].username
+            }
+            access_token = create_access_token(payload)
+        else:
+            access_token = "credenciales no validas"
+            
+        context = {
+            'status':True,
+            'content':access_token
+        }
+        
+        return context
         
         
 api.add_resource(UsuarioResource,'/usuario')
+api.add_resource(LoginResource,'/login')
